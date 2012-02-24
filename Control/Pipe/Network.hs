7b925752b1b4d3c322da798b80906fcd89fc462d
Paolo Capriotti <p.capriotti@gmail.com>
2012-02-24 14:48:05 +0000

    Port to finalizable pipes.

--- Control/Pipe/Network.hs
+++ Control/Pipe/Network.hs
@@ -13,33 +13,33 @@ import qualified Network.Socket as NS
 import Network.Socket (Socket)
 import Network.Socket.ByteString (sendAll, recv)
 import Data.ByteString (ByteString)
+import Data.Void
 import qualified Data.ByteString as B
 import Control.Concurrent (forkIO)
-import Control.Exception (bracketOnError, IOException, bracket, finally)
-import Control.Monad (forever)
-import Control.Monad.Trans (MonadIO, lift, liftIO)
+import qualified Control.Exception as E
+import Control.Monad (forever, unless)
+import Control.Monad.Trans (MonadIO, liftIO)
 import Control.Pipe
 
 -- adapted from conduit
 
 -- | Stream data from the socket.
-socketReader :: MonadIO m => Socket -> Pipe x ByteString m ()
+socketReader :: MonadIO m => Socket -> Pipe () ByteString m ()
 socketReader socket = go
   where
     go = do
       bs <- lift . liftIO $ recv socket 4096
-      if B.null bs
-        then return ()
-        else yield bs >> go
+      unless (B.null bs) $
+        yield bs >> go
 
 -- | Stream data to the socket.
-socketWriter :: MonadIO m => Socket -> Consumer ByteString m r
+socketWriter :: MonadIO m => Socket -> Pipe ByteString Void m r
 socketWriter socket = forever $ await >>= lift . liftIO . sendAll socket
 
 -- | A simple TCP application. It takes two arguments: the 'Producer' to read
 -- input data from, and the 'Consumer' to send output data to.
-type Application m r = Producer ByteString m ()
-                    -> Consumer ByteString m ()
+type Application m r = Pipe () ByteString m ()
+                    -> Pipe ByteString Void m ()
                     -> IO r
 
 -- | Settings for a TCP server. It takes a port to listen on, and an optional
@@ -53,7 +53,7 @@ data ServerSettings = ServerSettings
 -- new listening socket, accept connections on it, and spawn a new thread for
 -- each connection.
 runTCPServer :: MonadIO m => ServerSettings -> Application m r -> IO r
-runTCPServer (ServerSettings port host) app = bracket
+runTCPServer (ServerSettings port host) app = E.bracket
     (bindPort host port)
     NS.sClose
     (forever . serve)
@@ -61,7 +61,7 @@ runTCPServer (ServerSettings port host) app = bracket
     serve lsocket = do
       (socket, _addr) <- NS.accept lsocket
       forkIO $ do
-        finally
+        E.finally
           (app (socketReader socket) (socketWriter socket))
           (NS.sClose socket)
         return ()
@@ -74,7 +74,7 @@ data ClientSettings = ClientSettings
 
 -- | Run an 'Application' by connecting to the specified server.
 runTCPClient :: MonadIO m => ClientSettings -> Application m r -> IO r
-runTCPClient (ClientSettings port host) app = bracket
+runTCPClient (ClientSettings port host) app = E.bracket
     (getSocket host port)
     NS.sClose
     (\s -> app (socketReader s) (socketWriter s))
@@ -87,7 +87,7 @@ getSocket host' port' = do
                         , NS.addrSocketType = NS.Stream
                         }
     (addr:_) <- NS.getAddrInfo (Just hints) (Just host') (Just $ show port')
-    bracketOnError
+    E.bracketOnError
       (NS.socket (NS.addrFamily addr)
                  (NS.addrSocketType addr)
                  (NS.addrProtocol addr))
@@ -109,14 +109,13 @@ bindPort host p = do
         port = Just . show $ p
     addrs <- NS.getAddrInfo (Just hints) host port
     let
-        tryAddrs (addr1:rest@(_:_)) =
-                                      catch
+        tryAddrs (addr1:rest@(_:_)) = E.catch
                                       (theBody addr1)
-                                      (\(_ :: IOException) -> tryAddrs rest)
+                                      (\(_ :: E.IOException) -> tryAddrs rest)
         tryAddrs (addr1:[])         = theBody addr1
         tryAddrs _                  = error "bindPort: addrs is empty"
         theBody addr =
-          bracketOnError
+          E.bracketOnError
           (NS.socket
             (NS.addrFamily addr)
             (NS.addrSocketType addr)
