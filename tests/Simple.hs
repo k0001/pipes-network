8cdb0d8f19b8d313f2f8b65844a420289a7c1c95
Renzo Carbonara <gnuk0001@gmail.com>
2013-02-28 16:16:52 -0300

    Some more boring tests

--- tests/Simple.hs
+++ tests/Simple.hs
@@ -17,54 +17,110 @@ import qualified Control.Proxy.Safe      as P
 import qualified Control.Proxy.TCP       as T
 import qualified Control.Proxy.TCP.Safe  as T'
 
-host1  = "127.0.0.1"   :: String
-host1p = T.Host host1  :: T.HostPreference
-port1  = "14000"       :: NS.ServiceName
-msg1   = take 1000 $ cycle ["Hello ", "World\r\n"] :: [B.ByteString]
-msg1b  = B.concat msg1 :: B.ByteString
+host1  = "127.0.0.1"                        :: String
+host1p = T.Host host1                       :: T.HostPreference
+ports  = fmap show [14000..14010]           :: [NS.ServiceName]
+msg1   = take 1000 $ cycle ["Hell","o\r\n"] :: [B.ByteString]
+msg1b  = B.concat msg1                      :: B.ByteString
 
-serveWriteToConnectWrite :: Assertion
-serveWriteToConnectWrite = do
-    forkIO server
-    -- we sleep half second hoping that the server has started.
-    threadDelay 500000
-    client
+test_safe_serveWriteD_then_connectReadS :: Assertion
+test_safe_serveWriteD_then_connectReadS = do
+    done <- newEmptyMVar
+    forkIO $ server >> putMVar done ()
+    -- we sleep 200ms hoping that the server has started.
+    threadDelay 200000 >> client
+    takeMVar done
   where
+    port = ports !! 0
     server = do
-      let p = P.fromListS msg1 >-> T'.serveWriteD Nothing host1p port1
+      let p = P.fromListS msg1 >-> T'.serveWriteD Nothing host1p port
       P.runSafeIO . P.runProxy .P.runEitherK $ p
     client = do
-      let p = P.raiseK (T'.connectReadS Nothing 4096 host1 port1) >-> P.toListD
+      let p = P.raiseK (T'.connectReadS Nothing 4096 host1 port) >-> P.toListD
       (eex,out) <- P.trySafeIO . P.runWriterT . P.runProxy .P.runEitherK $ p
       case eex of
         Left ex  -> E.throw ex
         Right () -> B.concat out @=? msg1b
 
-connectWriteToServeRead :: Assertion
-connectWriteToServeRead = do
-    forkIO server
-    -- we sleep half second hoping that the server has started.
-    threadDelay 500000
-    client
+test_safe_serveReadS_then_connectWriteD :: Assertion
+test_safe_serveReadS_then_connectWriteD = do
+    done <- newEmptyMVar
+    forkIO $ server >> putMVar done ()
+    -- we sleep 200ms hoping that the server has started.
+    threadDelay 200000 >> client
+    takeMVar done
   where
+    port = ports !! 1
     client = do
-       let p = P.fromListS msg1 >-> T'.connectWriteD Nothing host1 port1
+       let p = P.fromListS msg1 >-> T'.connectWriteD Nothing host1 port
        P.runSafeIO . P.runProxy .P.runEitherK $ p
     server = do
-      let p = P.raiseK (T'.serveReadS Nothing 4096 host1p port1) >-> P.toListD
+      let p = P.raiseK (T'.serveReadS Nothing 4096 host1p port) >-> P.toListD
       (eex,out) <- P.trySafeIO . P.runWriterT . P.runProxy .P.runEitherK $ p
       case eex of
         Left ex  -> E.throw ex
         Right () -> B.concat out @=? msg1b
+
+test_listen_accept_socketReadS_then_connect_socketWriteD :: Assertion
+test_listen_accept_socketReadS_then_connect_socketWriteD = do
+    listening <- newEmptyMVar
+    done <- newEmptyMVar
+    forkIO $ server listening >> putMVar done ()
+    takeMVar listening >> client
+    takeMVar done
+  where
+    port = ports !! 2
+    server listening = do
+      T.listen host1p port $ \(lsock, laddr) -> do
+        putMVar listening ()
+        T.accept lsock $ \(csock, caddr) -> do
+          let p = P.raiseK (T.socketReadS 4096 csock) >-> P.toListD
+          ((), out) <- P.runWriterT . P.runProxy $ p
+          B.concat out @=? msg1b
+    client = do
+      T.connect host1 port $ \(csock, caddr) -> do
+        let p = P.fromListS msg1 >-> T.socketWriteD csock
+        P.runProxy p
+
+test_listen_accept_socketWriteD_then_connect_socketReadD :: Assertion
+test_listen_accept_socketWriteD_then_connect_socketReadD = do
+    listening <- newEmptyMVar
+    done <- newEmptyMVar
+    forkIO $ server listening >> putMVar done ()
+    takeMVar listening >> client
+    takeMVar done
+  where
+    port = ports !! 3
+    server listening = do
+      T.listen host1p port $ \(lsock, laddr) -> do
+        putMVar listening ()
+        T.accept lsock $ \(csock, caddr) -> do
+          let p = P.fromListS msg1 >-> T.socketWriteD csock
+          P.runProxy p
+    client = do
+      T.connect host1 port $ \(csock, caddr) -> do
+        let p = P.raiseK (T.socketReadS 4096 csock) >-> P.toListD
+        ((), out) <- P.runWriterT . P.runProxy $ p
+        B.concat out @=? msg1b
 
 tests :: [Test]
 tests =
-  [ testGroup "Safe TCP"
-    [ testGroup "{serve,connect}{WriteD,ReadS}"
-      [ testCase "serveWriteToConnectWrite" serveWriteToConnectWrite
-      , testCase "connectWriteToServeRead"  connectWriteToServeRead
+  [ testGroup "TCP"
+    [ testGroup "{listen+accept,connect} + {socketReadS,socketWriteD}"
+      [ testCase "test_listen_accept_socketReadS_then_connect_socketWriteD"
+                  test_listen_accept_socketReadS_then_connect_socketWriteD
+      , testCase "test_listen_accept_socketWriteD_then_connect_socketReadD"
+                  test_listen_accept_socketWriteD_then_connect_socketReadD
       ]
     ]
+ , testGroup "TCP.Safe"
+   [ testGroup "{serve,connect}{WriteD,ReadS}"
+     [ testCase "test_safe_serveWriteD_then_connectReadS"
+                test_safe_serveWriteD_then_connectReadS
+     , testCase "test_safe_serveReadS_then_connectWriteD"
+                test_safe_serveReadS_then_connectWriteD
+     ]
+   ]
   ]
 
 main :: IO ()
