60237220f368ec9d95ed0c38031b72b09dc6f22a
Renzo Carbonara <gnuk0001@gmail.com>
2013-08-25 21:42:11 -0300

    Cleaning up API to play well with the latest pipes-safe

--- src/Pipes/Network/TCP.hs
+++ src/Pipes/Network/TCP.hs
@@ -1,19 +1,14 @@
 {-# LANGUAGE RankNTypes #-}
 
 -- | This minimal module exports facilities that ease the usage of TCP
--- 'NS.Socket's in the /Pipes ecosystem/.
---
--- You are encouraged to use this module in conjunction with the
--- "Network.Simple.TCP" module from the @network-simple@ package:
---
--- @
--- import qualified Network.Simple.TCP as NT
--- import qualified Pipes.Network.TCP  as NT
--- @
+-- 'NS.Socket's in the /Pipes ecosystem/. It is meant to be used together with
+-- the "Network.Simple.TCP" module from the @network-simple@ package.
 --
 -- This module /does not/ export facilities that would allow you to acquire new
--- 'NS.Socket's within a 'Proxy' pipeline. If you need to do so, then you should
--- use the similar "Pipes.Network.TCP.Safe" module instead.
+-- 'NS.Socket's within a 'Proxy' pipeline. If you need to do so, then you'll
+-- need to rely on additional features exported the "Pipes.Network.TCP.Safe"
+-- module, which among other things, overrides some of the functions from
+-- "Network.Simple.TCP".
 
 module Pipes.Network.TCP (
   -- * Receiving
@@ -25,6 +20,7 @@ module Pipes.Network.TCP (
   , toSocket
   ) where
 
+import           Control.Monad.IO.Class         (MonadIO(liftIO))
 import qualified Data.ByteString                as B
 import qualified Network.Socket                 as NS
 import qualified Network.Socket.ByteString      as NSB
@@ -37,12 +33,12 @@ import           Pipes.Core
 --
 -- The following 'Proxy's allow you to receive bytes from the remote end.
 --
--- Besides the 'Proxy's exported below, you might want to 'lift'
+-- Besides the 'Proxy's exported below, you might want to 'liftIO'
 -- "Network.Simple.TCP"'s 'Network.Simple.TCP.recv' to be used as an 'Effect':
 --
 -- @
--- recv' :: 'NS.Socket' -> 'Int' -> 'Effect'' 'IO' ('Maybe' 'B.ByteString')
--- recv' sock nbytes = 'lift' $ 'Network.Simple.TCP.recv' sock nbytes
+-- recv' :: 'MonadIO' m => 'NS.Socket' -> 'Int' -> 'Effect'' m ('Maybe' 'B.ByteString')
+-- recv' sock nbytes = 'liftIO' $ 'Network.Simple.TCP.recv' sock nbytes
 -- @
 --
 -- This module doesn't export this small function so that you can enjoy
@@ -56,15 +52,17 @@ import           Pipes.Core
 --
 -- This 'Producer' returns if the remote peer closes its side of the connection
 -- or EOF is received.
-fromSocket :: NS.Socket  -- ^Connected socket.
-           -> Int        -- ^Maximum number of bytes to receive and send
-                         -- dowstream at once. Any positive value is fine, the
-                         -- optimal value depends on how you deal with the
-                         -- received data. Try using @4096@ if you don't care.
-           -> Producer B.ByteString IO ()
+fromSocket
+  :: MonadIO m
+  => NS.Socket  -- ^Connected socket.
+  -> Int        -- ^Maximum number of bytes to receive and send
+                -- dowstream at once. Any positive value is fine, the
+                -- optimal value depends on how you deal with the
+                -- received data. Try using @4096@ if you don't care.
+  -> Producer B.ByteString m ()
 fromSocket sock nbytes = loop where
     loop = do
-        bs <- lift (NSB.recv sock nbytes)
+        bs <- liftIO (NSB.recv sock nbytes)
         if B.null bs
            then return ()
            else respond bs >> loop
@@ -88,20 +86,22 @@ fromSocketN sock = loop where
 --
 -- The following 'Proxy's allow you to send bytes to the remote end.
 --
--- Besides the 'Proxy's below, you might want to 'lift' "Network.Simple.TCP"'s
+-- Besides the 'Proxy's below, you might want to 'liftIO' "Network.Simple.TCP"'s
 -- 'Network.Simple.TCP.send' to be used as an 'Effect':
 --
 -- @
--- send' :: 'NS.Socket' -> 'B.ByteString' -> 'Effect'' 'IO' ()
--- send' sock bytes = 'lift' $ 'Network.Simple.TCP.send' sock bytes
+-- send' :: 'MonadIO' m => 'NS.Socket' -> 'B.ByteString' -> 'Effect'' m ()
+-- send' sock bytes = 'liftIO' $ 'Network.Simple.TCP.send' sock bytes
 -- @
 --
 -- This module doesn't export this small function so that you can enjoy
 -- composing it yourself whenever you need it.
 
 -- | Sends to the remote end each 'B.ByteString' received from upstream.
-toSocket :: NS.Socket  -- ^Connected socket.
-         -> Consumer B.ByteString IO r
-toSocket sock = cat //> lift . NSB.sendAll sock
-{-# INLINABLE toSocket #-}
+toSocket
+  :: MonadIO m
+  => NS.Socket  -- ^Connected socket.
+  -> Consumer B.ByteString m r
+toSocket sock = cat //> liftIO . NSB.sendAll sock
+{-# INLINE toSocket #-}
 
--- src/Pipes/Network/TCP/Safe.hs
+++ src/Pipes/Network/TCP/Safe.hs
@@ -1,130 +1,135 @@
 {-# LANGUAGE Rank2Types #-}
 
--- | This module exports functions that allow you to safely use 'NS.Socket'
--- resources within a 'P.Proxy' pipeline, possibly acquiring and releasing such
--- resources within the pipeline itself, using the facilities provided by
--- the @pipes-safe@ library.
+-- | This module exports facilities allowing you to safely obtain, use and
+-- release 'NS.Socket' resources within a /Pipes/ pipeline, by relying on
+-- @pipes-safe@.
 --
--- Instead, if just want to use resources already acquired or released outside
--- the pipeline, then you could use the simpler functions exported by
--- "Control.Proxy.TCP".
+-- This module is meant to be used together with "Pipes.Network.TCP", and as
+-- such it overrides some functions from "Network.Simple.TCP" so that they use
+-- 'Ps.MonadSafe' instead of 'IO' as their base monad. It also exports
+-- 'Producer's and 'Consumer's that establish a TCP connection and interact
+-- with it in a streaming fashion at once.
+--
+-- If you just want to use 'NS.Socket' obtained outside the /Pipes/ pipeline,
+-- then you can just ignore this module and use the simpler functions exported
+-- by "Pipes.Network.TCP" directly.
 
 module Pipes.Network.TCP.Safe (
-  -- * Client side
-  -- $client-side
-  connect,
-  -- ** Streaming
+  -- * 'Ps.MonadSafe'-aware versions of some "Pipes.Network.TCP" functions
+  -- ** Client side
+    connect
+  -- ** Server side
+  , serve
+  -- *** Listening
+  , listen
+  -- *** Accepting
+  , accept
+  , acceptFork
+
+  -- * Streaming
+  -- ** Client side
   -- $client-streaming
-  connectRead,
-  connectWrite,
-
-  -- * Server side
-  -- $server-side
-  serve,
-  -- ** Listening
-  listen,
-  -- ** Accepting
-  accept,
-  acceptFork,
-  -- ** Streaming
+  , connectRead
+  , connectWrite
+  -- ** Server side
   -- $server-streaming
-  serveRead,
-  serveWrite,
-
-  -- * Socket streams
-  -- $socket-streaming
-  recv,
-  recv',
-  send,
-
-  -- * Note to Windows users
-  -- $windows-users
-  NS.withSocketsDo,
-
-
-  -- * Exports
-  S.HostPreference(..),
+  , serveRead
+  , serveWrite
   ) where
 
 import           Control.Concurrent             (ThreadId)
 import           Control.Monad
+import           Control.Monad.IO.Class         (MonadIO(liftIO))
 import qualified Data.ByteString                as B
-import           Data.Function                  (fix)
 import qualified Network.Socket                 as NS
 import qualified Network.Simple.TCP             as S
 import           Pipes
-import           Pipes.Core
 import qualified Pipes.Safe                     as Ps
+import qualified Pipes.Network.TCP              as PNT
 
 --------------------------------------------------------------------------------
 
--- $windows-users
---
--- If you are running Windows, then you /must/ call 'NS.withSocketsDo', just
--- once, right at the beginning of your program. That is, change your program's
--- 'main' function from:
---
--- @
--- main = do
---   print \"Hello world\"
---   -- rest of the program...
--- @
---
--- To:
---
--- @
--- main = 'NS.withSocketsDo' $ do
---   print \"Hello world\"
---   -- rest of the program...
--- @
---
--- If you don't do this, your networking code won't work and you will get many
--- unexpected errors at runtime. If you use an operating system other than
--- Windows then you don't need to do this, but it is harmless to do it, so it's
--- recommended that you do for portability reasons.
-
---------------------------------------------------------------------------------
-
--- $client-side
---
--- Here's how you could run a TCP client:
---
--- @
--- 'connect' \"www.example.org\" \"80\" $ \(connectionSocket, remoteAddr) -> do
---   putStrLn $ \"Connection established to \" ++ show remoteAddr
---   -- Now you may use connectionSocket as you please within this scope,
---   -- possibly using 'recv', 'send' or similar proxies
---   -- explained below.
--- @
---
--- You might instead prefer the simpler but less general solutions offered by
--- 'connectRead' and 'connectWrite', so check those too.
-
---------------------------------------------------------------------------------
-
--- | Connect to a TCP server and use the connection.
---
--- The connection socket is closed when done or in case of exceptions.
---
--- If you prefer to acquire close the socket yourself, then use
--- 'S.connectSock' and the 'NS.sClose' from "Network.Socket" instead.
+-- | Like 'S.connect' from "Network.Simple.TCP", except using 'Ps.MonadSafe'.
 connect
   :: Ps.MonadSafe m
   => NS.HostName                   -- ^Server hostname.
   -> NS.ServiceName                -- ^Server service port.
-  -> ((NS.Socket, NS.SockAddr) -> Proxy a' a b' b m r)
+  -> ((NS.Socket, NS.SockAddr) -> m r)
                                    -- ^Computation taking the
                                    -- communication socket and the server
                                    -- address.
-  -> Proxy a' a b' b m r
+  -> m r
 connect host port = Ps.bracket (S.connectSock host port) (NS.sClose . fst)
+
+--------------------------------------------------------------------------------
+
+-- | Like 'S.serve' from "Network.Simple.TCP", except using 'Ps.MonadSafe'.
+serve
+  :: Ps.MonadSafe m
+  => S.HostPreference              -- ^Preferred host to bind.
+  -> NS.ServiceName                -- ^Service port to bind.
+  -> ((NS.Socket, NS.SockAddr) -> IO ())
+                                   -- ^Computation to run in a different thread
+                                   -- once an incoming connection is accepted.
+                                   -- Takes the connection socket and remote end
+                                   -- address.
+  -> m r
+serve hp port k = do
+   listen hp port $ \(lsock,_) -> do
+      forever $ acceptFork lsock k
+
+--------------------------------------------------------------------------------
+
+-- | Like 'S.listen' from "Network.Simple.TCP", except using 'Ps.MonadSafe'.
+listen
+  :: Ps.MonadSafe m
+  => S.HostPreference              -- ^Preferred host to bind.
+  -> NS.ServiceName                -- ^Service port to bind.
+  -> ((NS.Socket, NS.SockAddr) -> m r)
+                                   -- ^Computation taking the listening
+                                   -- socket and the address it's bound to.
+  -> m r
+listen hp port = Ps.bracket listen' (NS.sClose . fst)
+  where
+    listen' = do x@(bsock,_) <- S.bindSock hp port
+                 NS.listen bsock (max 2048 NS.maxListenQueue)
+                 return x
+
+--------------------------------------------------------------------------------
+
+-- | Like 'S.accept' from "Network.Simple.TCP", except using 'Ps.MonadSafe'.
+accept
+  :: Ps.MonadSafe m
+  => NS.Socket                     -- ^Listening and bound socket.
+  -> ((NS.Socket, NS.SockAddr) -> m r)
+                                   -- ^Computation to run once an incoming
+                                   -- connection is accepted. Takes the
+                                   -- connection socket and remote end address.
+  -> m r
+accept lsock k = do
+    conn@(csock,_) <- liftIO (NS.accept lsock)
+    Ps.finally (k conn) (NS.sClose csock)
+{-# INLINABLE accept #-}
+
+-- | Like 'S.acceptFork' from "Network.Simple.TCP", except using 'Ps.MonadSafe'.
+acceptFork
+  :: Ps.MonadSafe m
+  => NS.Socket                     -- ^Listening and bound socket.
+  -> ((NS.Socket, NS.SockAddr) -> IO ())
+                                  -- ^Computation to run in a different thread
+                                  -- once an incoming connection is accepted.
+                                  -- Takes the connection socket and remote end
+                                  -- address.
+  -> m ThreadId
+acceptFork lsock k = liftIO (S.acceptFork lsock k)
+{-# INLINE acceptFork #-}
 
 --------------------------------------------------------------------------------
 
 -- $client-streaming
 --
 -- The following proxies allow you to easily connect to a TCP server and
--- immediately interact with it using streams, all at once, instead of
+-- immediately interact with it in a streaming fashion, all at once, instead of
 -- having to perform the individual steps separately.
 
 --------------------------------------------------------------------------------
@@ -150,12 +155,12 @@ connectRead
   -> Producer B.ByteString m ()
 connectRead nbytes host port = do
    connect host port $ \(csock,_) -> do
-     recv csock nbytes
+      PNT.fromSocket csock nbytes
 
 -- | Connects to a TCP server, sends to the remote end the bytes received from
 -- upstream.
 --
--- The connection socket is closed when done or in case of exceptions.
+-- The connection socket is closed in case of exceptions.
 --
 -- Using this proxy you can write straightforward code like the following, which
 -- greets a TCP client listening locally at port 9000:
@@ -169,119 +174,15 @@ connectWrite
   -> () -> Consumer B.ByteString m r
 connectWrite hp port = \() -> do
    connect hp port $ \(csock,_) -> do
-     forever $ await //> send csock
-
---------------------------------------------------------------------------------
-
--- $server-side
---
--- Here's how you can run a TCP server that handles in different threads each
--- incoming connection to port @8000@ at IPv4 address @127.0.0.1@:
---
--- @
--- 'serve' ('Host' \"127.0.0.1\") \"8000\" $ \(connectionSocket, remoteAddr) -> do
---   putStrLn $ \"TCP connection established from \" ++ show remoteAddr
---   -- Now you may use connectionSocket as you please within this scope,
---   -- possibly using 'recv', 'send' or similar proxies
---   -- explained below.
--- @
---
--- You might instead prefer the simpler but less general solutions offered by
--- 'serveRead' and 'serveWrite', so check those too. On the other hand,
--- if you need more control on the way your server runs, then you can use more
--- advanced functions such as 'listen', 'accept' and 'acceptFork'.
-
---------------------------------------------------------------------------------
-
--- | Start a TCP server that accepts incoming connections and handles each of
--- them concurrently in different threads.
---
--- Any acquired network resources are properly closed and discarded when done or
--- in case of exceptions.
---
--- Note: This function performs 'listen' and 'acceptFork', so you don't need to
--- perform those manually.
-serve
-  :: Ps.MonadSafe m
-  => S.HostPreference              -- ^Preferred host to bind.
-  -> NS.ServiceName                -- ^Service port to bind.
-  -> ((NS.Socket, NS.SockAddr) -> IO ())
-                                   -- ^Computation to run in a different thread
-                                   -- once an incoming connection is accepted.
-                                   -- Takes the connection socket and remote end
-                                   -- address.
-  -> Proxy a' a b' b m r
-serve hp port k = do
-   listen hp port $ \(lsock,_) -> do
-     forever $ acceptFork lsock k
-
---------------------------------------------------------------------------------
-
--- | Bind a TCP listening socket and use it.
---
--- The listening socket is closed when done or in case of exceptions.
---
--- If you prefer to acquire and close the socket yourself, then use
--- 'S.bindSock' and the 'NS.listen' and 'NS.sClose' functions from
--- "Network.Socket" instead.
---
--- Note: 'N.maxListenQueue' is tipically 128, which is too small for high
--- performance servers. So, we use the maximum between 'N.maxListenQueue' and
--- 2048 as the default size of the listening queue.
-listen
-  :: Ps.MonadSafe m
-  => S.HostPreference              -- ^Preferred host to bind.
-  -> NS.ServiceName                -- ^Service port to bind.
-  -> ((NS.Socket, NS.SockAddr) -> Proxy a' a b' b m r)
-                                   -- ^Computation taking the listening
-                                   -- socket and the address it's bound to.
-  -> Proxy a' a b' b m r
-listen hp port = Ps.bracket listen' (NS.sClose . fst)
-  where
-    listen' = do x@(bsock,_) <- S.bindSock hp port
-                 NS.listen bsock $ max 2048 NS.maxListenQueue
-                 return x
-
---------------------------------------------------------------------------------
-
--- | Accept a single incoming connection and use it.
---
--- The connection socket is closed when done or in case of exceptions.
-accept
-  :: Ps.MonadSafe m
-  => NS.Socket                     -- ^Listening and bound socket.
-  -> ((NS.Socket, NS.SockAddr) -> Proxy a' a b' b m r)
-                                   -- ^Computation to run once an incoming
-                                   -- connection is accepted. Takes the
-                                   -- connection socket and remote end address.
-  -> Proxy a' a b' b m r
-accept lsock k = do
-    conn@(csock,_) <- lift . Ps.tryIO $ NS.accept lsock
-    Ps.finally (k conn) (NS.sClose csock)
-{-# INLINABLE accept #-}
-
--- | Accept a single incoming connection and use it in a different thread.
---
--- The connection socket is closed when done or in case of exceptions.
-acceptFork
-  :: Ps.MonadSafe m
-  => NS.Socket                     -- ^Listening and bound socket.
-  -> ((NS.Socket, NS.SockAddr) -> IO ())
-                                  -- ^Computation to run in a different thread
-                                  -- once an incoming connection is accepted.
-                                  -- Takes the connection socket and remote end
-                                  -- address.
-  -> Proxy a' a b' b m ThreadId
-acceptFork lsock k = Ps.tryIO $ S.acceptFork lsock k
-{-# INLINABLE acceptFork #-}
+      PNT.toSocket csock
 
 --------------------------------------------------------------------------------
 
 -- $server-streaming
 --
 -- The following proxies allow you to easily run a TCP server and immediately
--- interact with incoming connections using streams, all at once, instead of
--- having to perform the individual steps separately.
+-- interact with incoming connections in a streaming fashion, all at once,
+-- instead of having to perform the individual steps separately.
 
 --------------------------------------------------------------------------------
 
@@ -313,8 +214,8 @@ serveRead
   -> () -> Producer B.ByteString m ()
 serveRead nbytes hp port () = do
    listen hp port $ \(lsock,_) -> do
-     accept lsock $ \(csock,_) -> do
-       recv csock nbytes
+      accept lsock $ \(csock,_) -> do
+         PNT.fromSocket csock nbytes
 
 -- | Binds a listening socket, accepts a single connection, sends to the remote
 -- end the bytes received from upstream.
@@ -334,58 +235,7 @@ serveWrite
   -> () -> Consumer B.ByteString m r
 serveWrite hp port = \() -> do
    listen hp port $ \(lsock,_) -> do
-     accept lsock $ \(csock,_) -> do
-       forever $ await //> send csock
-
---------------------------------------------------------------------------------
-
--- $socket-streaming
---
--- Once you have a connected 'NS.Socket', you can use the following 'P.Proxy's
--- to interact with the other connection end using streams.
-
---------------------------------------------------------------------------------
-
--- | Receives bytes from the remote end and sends them downstream.
---
--- Less than the specified maximum number of bytes might be received at once.
---
--- This proxy returns if the remote peer closes its side of the connection or
--- EOF is received.
-recv
-  :: Ps.MonadSafe m
-  => NS.Socket          -- ^Connected socket.
-  -> Int                -- ^Maximum number of bytes to receive and send
-                        -- dowstream at once. Any positive value is fine, the
-                        -- optimal value depends on how you deal with the
-                        -- received data. Try using @4096@ if you don't care.
-  -> Producer B.ByteString m ()
-recv sock = \nbytes -> fix $ \loop -> do
-    mbs <- lift . Ps.tryIO $ S.recv sock nbytes
-    case mbs of
-      Just bs -> respond bs >> loop
-      Nothing -> return ()
-{-# INLINABLE recv #-}
-
--- | Just like 'recv', except each request from downstream specifies the
--- maximum number of bytes to receive.
-recv'
-  :: Ps.MonadSafe m
-  => NS.Socket -> Int -> Server Int B.ByteString m ()
-recv' sock = fix $ \loop nbytes -> do
-    mbs <- lift . Ps.tryIO $ S.recv sock nbytes
-    case mbs of
-      Just bs -> respond bs >>= loop
-      Nothing -> return ()
-{-# INLINABLE recv' #-}
-
--- | Sends to the remote end the bytes received from upstream.
-send
-  :: Ps.MonadSafe m
-  => NS.Socket          -- ^Connected socket.
-  -> B.ByteString       -- ^Bytes to send to the remote end.
-  -> Effect' m ()
-send sock = lift . Ps.tryIO . S.send sock
-{-# INLINABLE send #-}
+      accept lsock $ \(csock,_) -> do
+         PNT.toSocket csock
 
 
