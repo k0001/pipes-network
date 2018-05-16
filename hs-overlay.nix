f0287f7d11f978b65b57cdd42ebb32b154048c6e
Renzo Carbonara <gnuk0001@gmail.com>
2018-05-16 19:56:21 +0200

    docs

--- hs-overlay.nix
+++ hs-overlay.nix
@@ -1,13 +1,13 @@
 { pkgs }:
 
 let
-hs = pkgs.haskell.lib;
 src-network-simple = builtins.fetchGit {
   url = "https://github.com/k0001/network-simple";
   rev = "14e5517c01286c5f59db6990be8fc3366aa54294";
 };
 
 in
+# This expression can be used as a Haskell package set `packageSetConfig`:
 pkgs.lib.composeExtensions
   (import "${src-network-simple}/hs-overlay.nix")
   (self: super: {
