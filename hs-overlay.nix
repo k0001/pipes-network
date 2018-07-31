9268b3a42856889fe3c189e271297be9967be9d6
Renzo Carbonara <gnuk0001@gmail.com>
2018-07-31 21:03:47 +0300

    bump network-simple version

--- hs-overlay.nix
+++ hs-overlay.nix
@@ -3,13 +3,13 @@
 let
 src-network-simple = builtins.fetchGit {
   url = "https://github.com/k0001/network-simple";
-  rev = "14e5517c01286c5f59db6990be8fc3366aa54294";
+  rev = "2c3ab6e7aa2a86be692c55bf6081161d83d50c34";
 };
 
 in
 # This expression can be used as a Haskell package set `packageSetConfig`:
 pkgs.lib.composeExtensions
-  (import "${src-network-simple}/hs-overlay.nix")
+  (import "${src-network-simple}/hs-overlay.nix" { inherit pkgs; })
   (self: super: {
      pipes-network = super.callPackage ./pkg.nix {};
   })
